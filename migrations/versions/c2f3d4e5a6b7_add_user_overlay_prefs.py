b5c8ce82d13b0fb3b1f54b5cd6d7291309d61e54
Gaute Lindkvist <lindkvis@gmail.com>
2026-01-29 18:12:18 +0100

    Fix merge issues with alembic

--- migrations/versions/c2f3d4e5a6b7_add_user_overlay_prefs.py
+++ migrations/versions/c2f3d4e5a6b7_add_user_overlay_prefs.py
@@ -14,7 +14,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = "c2f3d4e5a6b7"
-down_revision: Union[str, None] = "b7c2c9f6c0d1"
+down_revision: Union[str, None] = "e3f2a4b5c6d7"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
