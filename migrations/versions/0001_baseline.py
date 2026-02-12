a2b6ec5af7db21e56e93c3b48832a4a4102fa33e
Gaute Lindkvist <glindkvist@kontur.tech>
2026-02-12 12:39:57 +0100

    Squash migrations

---
+++ migrations/versions/0001_baseline.py
@@ -0,0 +1,29 @@
+"""baseline schema
+
+Revision ID: 0001_baseline
+Revises: 
+Create Date: 2026-02-12 00:00:00.000000
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+from sqlmodel import SQLModel
+
+# Import all models to register metadata.
+import pixlvault.db_models  # noqa: F401
+
+
+# revision identifiers, used by Alembic.
+revision: str = "0001_baseline"
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    SQLModel.metadata.create_all(op.get_bind())
+
+
+def downgrade() -> None:
+    SQLModel.metadata.drop_all(op.get_bind())
